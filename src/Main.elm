cc4e733eded1826e0f957034e06263fcd3816cad
sanjana-mishra <sanjana.mishra99@gmail.com>
2022-06-02 14:57:58 +0530

    Included "your code" and make the code as per MacRocks requirements

--- src/Main.elm
+++ src/Main.elm
@@ -187,6 +187,7 @@ myShapes model =
     , animationButton model |> move ( 112.5, 56 ) |> notifyTap AnimationToggle
     , GraphicSVG.text "CLT" |> fixedwidth |> bold |> GraphicSVG.size 9 |> filled (GraphicSVG.hsl 10 1.0 (abs (sin (1 * model.time)) + 0.2)) |> move ( -130, 55 )
     , GraphicSVG.text "Creator" |> fixedwidth |> bold |> GraphicSVG.size 4 |> filled (GraphicSVG.hsl 10 1.0 (abs (sin (1 * model.time)) + 0.2)) |> move ( -130, 50 )
+    , GraphicSVG.text "Your Code" |> fixedwidth |> bold |> GraphicSVG.size 4 |> filled white |> move ( -130, 32 )
     , yourCode model
     , if model.elevation == Angle.degrees 90 && model.focusAt /= Point3d.meters 0 0 0 then
         cutterUI model
@@ -198,11 +199,11 @@ myShapes model =
 
 yourCode model =
     group
-        [ rect 40 60
+        [ rect 50 80
             |> filled white
             |> makeTransparent 0.8
             |> addOutline (solid 0.3) darkBlue
-            |> move ( -110, -20 )
+            |> move ( -105, -10 )
         , if model.isCut then
             codeGen model.cltList
 
@@ -218,7 +219,7 @@ codeGen cltList =
             List.map2 (\y clt -> clt |> move ( 0, -y )) [ 0 ]
 
          else
-            List.map2 (\y clt -> clt |> move ( 0, -y )) [ 0, 12, 24, 36 ]
+            List.map2 (\y clt -> clt |> move ( 0, -y )) [ 0, 20, 40, 60 ]
         )
         <|
             List.map
@@ -229,38 +230,61 @@ codeGen cltList =
                             |> selectable
                             |> customFont "monospace"
                             |> filled darkBlue
-                            |> move ( -128, 6 )
+                            |> move ( -129, 27 )
+                        , GraphicSVG.text (reqCode1)
+                            |> GraphicSVG.size 2.5
+                            |> selectable
+                            |> customFont "monospace"
+                            |> filled darkBlue
+                            |> move ( -125, 24 )
+                        , GraphicSVG.text (reqCode2 clt)
+                            |> GraphicSVG.size 2.5
+                            |> selectable
+                            |> customFont "monospace"
+                            |> filled darkBlue
+                            |> move ( -125, 21 )
                         , GraphicSVG.text (plankRot "X" clt)
                             |> GraphicSVG.size 2.5
                             |> selectable
                             |> customFont "monospace"
                             |> filled darkBlue
-                            |> move ( -122, 3 )
+                            |> move ( -125, 18 )
                         , GraphicSVG.text (plankRot "Y" clt)
                             |> GraphicSVG.size 2.5
                             |> selectable
                             |> customFont "monospace"
                             |> filled darkBlue
-                            |> move ( -122, 0 )
+                            |> move ( -125, 15 )
                         , GraphicSVG.text (plankRot "Z" clt)
                             |> GraphicSVG.size 2.5
                             |> selectable
                             |> customFont "monospace"
                             |> filled darkBlue
-                            |> move ( -122, -3 )
+                            |> move ( -125, 12 )
                         ]
                 )
                 cltList
 
 
-plankMeshCode clt =
-    "CltPlank "
+plankMeshCode clt = 
+    "createCltPlank "
         ++ Debug.toString clt.width
         ++ " "
         ++ Debug.toString clt.length
         ++ " "
         ++ Debug.toString clt.height
 
+reqCode1 = 
+        "|> renderCltPlank"         
+        
+reqCode2 clt = 
+        "|> fromEntity ("
+        ++ Debug.toString clt.width
+        ++ ", "
+        ++ Debug.toString clt.length
+        ++ ", "
+        ++ Debug.toString clt.height
+        ++ ")"
 
 plankRot dir clt =
     case dir of
@@ -269,27 +293,31 @@ plankRot dir clt =
                 ""
 
             else
-                "|> RotX "
+                "|> rotateX3D (degrees "
                     ++ Debug.toString (Angle.inDegrees clt.rotationAngleX)
+                    ++ ")"
 
         "Y" ->
             if clt.rotationAngleY == Angle.degrees 0 then
                 ""
 
             else
-                "|> RotY "
+                "|> rotateY3D (degrees "
                     ++ Debug.toString (Angle.inDegrees clt.rotationAngleY)
+                    ++ ")"
 
         "Z" ->
             if clt.rotationAngleZ == Angle.degrees 0 then
                 ""
 
             else
-                "|> RotZ "
+                 "|> rotateZ3D (degrees "
                     ++ Debug.toString (Angle.inDegrees clt.rotationAngleZ)
+                    ++ ")"
 
         _ ->
             ""
+
 
 
 
